b31ee20f75fd7bacc334f0fc20a381f29bf9020e
shforeal <rivena14798@gmail.com>
2022-09-01 15:32:02 +0300

    test

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get('/**', (req, res) => {
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
-http.listen(port, () => {
+app.listen(port, () => {
   logger.info('Server is running on port: ' + port)
 })
